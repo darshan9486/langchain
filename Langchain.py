7aecf3cdc239f23ce828b90ce4cd0955984af551
darshan9486 <darshan9486@gmail.com>
2023-04-02 16:10:46 +0100

    Add files via upload

---
+++ Langchain.py
@@ -0,0 +1,32 @@
+import os, streamlit as st
+
+# Uncomment to specify your OpenAI API key here (local testing only, not in production!), or add corresponding environment variable (recommended)
+os.environ['OPENAI_API_KEY']= "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"
+
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper
+from langchain import OpenAI
+
+# This example uses text-davinci-003 by default; feel free to change if desired
+llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+
+# Configure prompt parameters and initialise helper
+max_input_size = 4096
+num_output = 256
+max_chunk_overlap = 20
+
+prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+
+# Load documents from the 'data' directory
+documents = SimpleDirectoryReader('/Users/darshanvaidya/Downloads/Context').load_data()
+index = GPTSimpleVectorIndex.from_documents(documents)
+
+# llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+
+# Define a simple Streamlit app
+st.title("Ask Llama")
+query = st.text_input("What would you like to ask?", "")
+
+if st.button("Submit"):
+    response = index.query(query)
+    st.write(response)
